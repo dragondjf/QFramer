deab921daf497e28e1800b94b2a252b9ffa835a7
dragondjf <dragondjf@gmail.com>
2014-07-14 23:21:04 +0800

    add qtreewidget and qprogressbar

--- functionpages/uielement.cpp
+++ functionpages/uielement.cpp
@@ -7,7 +7,11 @@
 #include<QPushButton>
 #include<QRadioButton>
 #include<QCheckBox>
-
+#include<QTreeWidget>
+#include<QSplitter>
+#include<QHeaderView>
+#include<QProgressBar>
+#include<QTimer>
 UIElement::UIElement(QWidget *parent) :
     FTabWidget(parent)
 {
@@ -19,7 +23,9 @@ void UIElement::initUI()
 {
     initBaseElements();
     initTableWidget();
-    for(int i = 1; i< 9; ++i)
+    initTreeWidget();
+    initProgressBar();
+    for(int i = 1; i< 8; ++i)
     {
         QLabel* label = new QLabel(QString::number(i));
         addWidget(tr("Base UI Element %1").arg(QString::number(i)), QString("NavgationTab"), label);
@@ -103,15 +109,120 @@ void UIElement::initBaseElements()
 void UIElement::initTableWidget()
 {
     FTableWidget* table = new FTableWidget;
-    addWidget(tr("FTabelWidget"), QString("NavgationTab"), table);
+    addWidget(tr("TabelWidget"), QString("NavgationTab"), table);
 }
 
 void UIElement::initTreeWidget()
 {
+    QTreeWidget *treeWidget = new QTreeWidget();
+    treeWidget->setHeaderHidden(true);
+    treeWidget->setColumnCount(1);
+    QList<QTreeWidgetItem *> items;
+    for (int i = 0; i < 10; ++i)
+    {
+        items.append(new QTreeWidgetItem((QTreeWidget*)0, QStringList(QString("item: %1").arg(i))));
+        for(int j=0; j <10; j++)
+        {
+            items.at(i)->addChild(new QTreeWidgetItem((QTreeWidget*)0, QStringList(QString("child: %1").arg(j))));
+        }
+    }
+    treeWidget->insertTopLevelItems(0, items);
 
+    QTreeWidget *treeWidget2 = new QTreeWidget();
+
+    QStringList labels;
+    labels << tr("rpcID") << tr("rpcType") << tr("rpcDetail");
+    treeWidget2->setHeaderLabels(labels);
+    treeWidget2->setColumnCount(3);
+    treeWidget2->header()->setSectionResizeMode(0, QHeaderView::Fixed);
+    treeWidget2->header()->setSectionResizeMode(1, QHeaderView::Fixed);
+    treeWidget2->header()->setSectionResizeMode(2, QHeaderView::Stretch);
+    QList<QTreeWidgetItem *> items2;
+    for (int i = 0; i < 10; ++i)
+    {
+        items2.append(new QTreeWidgetItem((QTreeWidget*)0, QStringList(QString("item: %1").arg(i))));
+        for(int j=0; j <10; j++)
+        {
+            QStringList childs;
+            childs << tr("%1").arg(j) << tr("alert") << tr("show warning");
+            items2.at(i)->addChild(new QTreeWidgetItem((QTreeWidget*)0, childs));
+        }
+    }
+    treeWidget2->insertTopLevelItems(0, items2);
+
+
+    QTreeWidget *treeWidget3 = new QTreeWidget();
+    QStringList labels3;
+    labels3 << tr("rpcID") << tr("rpcType") << tr("rpcDetail");
+    treeWidget3->setHeaderLabels(labels3);
+    treeWidget3->setColumnCount(3);
+    treeWidget3->header()->setSectionResizeMode(0, QHeaderView::Fixed);
+    treeWidget3->header()->setSectionResizeMode(1, QHeaderView::Fixed);
+    treeWidget3->header()->setSectionResizeMode(2, QHeaderView::Stretch);
+    QList<QTreeWidgetItem *> items3;
+    for (int i = 0; i < 10; ++i)
+    {
+        QTreeWidgetItem* fitem = new QTreeWidgetItem((QTreeWidget*)0, QStringList(QString("item: %1").arg(i)));
+        fitem->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsSelectable |Qt::ItemIsEnabled);
+        fitem->setCheckState(0, Qt::Checked);
+        items3.append(fitem);
+        for(int j=0; j <10; j++)
+        {
+            QStringList childs3;
+            childs3 << tr("%1").arg(j) << tr("alert") << tr("show warning");
+            QTreeWidgetItem* sitem = new QTreeWidgetItem((QTreeWidget*)0, childs3);
+            sitem->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsSelectable |Qt::ItemIsEnabled);
+            sitem->setCheckState(0, Qt::Checked);
+            items3.at(i)->addChild(sitem);
+        }
+    }
+    treeWidget3->insertTopLevelItems(0, items3);
+
+
+
+    QSplitter* splitter = new QSplitter(this);
+    splitter->addWidget(treeWidget);
+    splitter->addWidget(treeWidget2);
+
+    QSplitter* vsplitter = new QSplitter(Qt::Vertical, this);
+    vsplitter->addWidget(splitter);
+    vsplitter->addWidget(treeWidget3);
+
+    addWidget(tr("TreeWidget"), QString("NavgationTab"), vsplitter);
 }
 
-void UIElement::initComoBox()
+void UIElement::initProgressBar()
 {
+    QWidget* progressBarsWidget = new QWidget;
+    QVBoxLayout* progressLayout = new QVBoxLayout;
+    for(int i=1; i< 11; ++i)
+    {
+        QProgressBar* p = new QProgressBar;
+        p->setRange(0, 100);
+        p->setValue(i * 10);
+        progressLayout->addWidget(p);
+        progressBars.append(p);
+    }
+    progressBarsWidget->setLayout(progressLayout);
+    addWidget(tr("ProgressBar"), QString("NavgationTab"), progressBarsWidget);
+    QTimer * ptimer = new QTimer;
+    ptimer->setInterval(20);
+    connect(ptimer, SIGNAL(timeout()), this, SLOT(updateProgressBar()));
+    ptimer->start();
+}
 
+void UIElement::updateProgressBar()
+{
+   for(int i=0; i< progressBars.size(); ++i)
+   {
+       int value = progressBars.at(i)->value();
+       if(value < 100)
+       {
+           progressBars.at(i)->setValue(value + 1);
+       }
+       else
+       {
+           progressBars.at(i)->setValue(0);
+       }
+   }
 }
--- functionpages/uielement.h
+++ functionpages/uielement.h
@@ -2,22 +2,24 @@
 #define UIELEMENT_H
 
 #include <QTabWidget>
+#include <QProgressBar>
 #include "QFramer/ftabwidget.h"
 class UIElement : public FTabWidget
 {
     Q_OBJECT
-
+private:
+    QList<QProgressBar *> progressBars;
 public:
     explicit UIElement(QWidget *parent = 0);
     void initUI();
     void initBaseElements();
     void initTableWidget();
     void initTreeWidget();
-    void initComoBox();
+    void initProgressBar();
 signals:
 
 public slots:
-
+    void updateProgressBar();
 };
 
 #endif // UIELEMENT_H
