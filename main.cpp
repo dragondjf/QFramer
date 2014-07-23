35c333fa459099373d5aa7b53cd03537b4849549
dragondjf <dragondjf@gmail.com>
2014-07-24 00:16:41 +0800

    delete wrong code con't setstylesheet beyfor mainwindow

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ int main(int argc, char *argv[])
     QApplication::setOrganizationDomain("https://github.com/dragondjf");
     QApplication::setApplicationName("QFramer");
 
-    setSkinForApp(QString(":/qss/skin/qss/fblue.qss"));
     MainWindow *main = MainWindow::getInstance();
     setSkinForApp(QString(":/qss/skin/qss/main.qss"));
     main->setAttribute(Qt::WA_DeleteOnClose);
