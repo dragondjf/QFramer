b93388405d9b2b41ff349bde5f597bbf1409c222
dragondjf <dragondjf@gmail.com>
2014-07-28 23:44:48 +0800

    update deploywin.py

--- deploywin.py
+++ deploywin.py
@@ -9,7 +9,8 @@ try:
 except ImportError:
     import xml.etree.ElementTree as ET
 
-distWinPath = os.sep.join([os.getcwd(), 'distwin'])
+workspacePath = os.getcwd()
+destinationPath = os.sep.join([os.getcwd(), 'distwin'])
 
 def delete_file_folder(src):
     '''delete files and folders'''
@@ -27,6 +28,15 @@ def delete_file_folder(src):
         except Exception, e:
             print e
 
+def getFilesBySuffix(rootpath, suffix):
+    fnames = []
+    for d, fd, fl in os.walk(rootpath):
+        if d == rootpath:
+            for item in fl:
+                if item.endswith(suffix):
+                    fnames.append(os.sep.join([rootpath, item]))
+    return fnames
+
 if __name__ == '__main__':
     buildPaths = []
     tree = ET.parse('QCFramer.pro.user')
@@ -37,10 +47,16 @@ if __name__ == '__main__':
                 for cccchild in ccchild:
                     if cccchild.attrib['key'] == "ProjectExplorer.BuildConfiguration.BuildDirectory":
                         buildPaths.append(cccchild.text)
-    delete_file_folder(distWinPath)
+    delete_file_folder(destinationPath)
     for item in ['distwin']:
         os.mkdir(os.sep.join([os.getcwd(), item]))
     exePath = os.sep.join([buildPaths[1], 'release', 'QCFramer.exe'])
-    shutil.copyfile(exePath, os.sep.join([distWinPath, 'QCFramer.exe']))
-    os.chdir(distWinPath)
+    shutil.copyfile(exePath, os.sep.join([destinationPath, 'QCFramer.exe']))
+    os.chdir(destinationPath)
     subprocess.call(["windeployqt.exe", "QCFramer.exe"])
+
+    for item in getFilesBySuffix(destinationPath, '.qm'):
+        delete_file_folder(item)
+
+    for item in ['qss']:
+        shutil.copytree(os.sep.join([workspacePath, 'skin', item]), os.sep.join([destinationPath ,item]))
